a092042594ba16f7a730d00dfde3c623124b2825
datodvp <gaguadato98@gmail.com>
2023-02-06 16:07:19 +0400

    textinput shortened

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,61 +1,50 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../../context/UserContext';
+import React from 'react';
+import { Field } from 'formik';
 import SuccessImg from '../../assets/images/SuccessImg.png';
 import FailureImg from '../../assets/images/FailureImg.png';
 import './styles.scss';
 
-const TextInput = ({ title, name, placeholder, hint, size }) => {
-  // checks if text input should be small or large
-  switch (size) {
-    case 'small':
-      size = 371;
-      break;
-    case 'large':
-      size = 798;
-      break;
-    default:
-      size = 798;
-  }
-
-  const { formik } = useContext(UserContext);
-
-  return (
-    <label
-      className={`text-input-label ${
-        formik.touched[name] && formik.errors[name]
-          ? 'failure'
-          : formik.touched[name] && !formik.errors[name] && 'success'
-      }`}
-      style={{ width: size }}
-    >
-      {title}
-      <div className="input-container">
-        <input
-          className={
-            formik.touched[name] && formik.errors[name]
-              ? 'failure'
-              : formik.touched[name] && !formik.errors[name] && 'success'
-          }
-          type="text"
-          placeholder={placeholder}
-          value={formik.values[name]}
-          onChange={formik.handleChange}
-          onFocus={formik.handleBlur}
-          name={name}
-        ></input>
-        {/* validation icons of input */}
-        {formik.touched[name] && formik.errors[name] ? (
-          <img className="failure" src={FailureImg} alt="failure"></img>
-        ) : (
-          formik.touched[name] &&
-          !formik.errors[name] && (
-            <img className="success" src={SuccessImg} alt="success"></img>
-          )
-        )}
-      </div>
-      <span className="hint">{hint}</span>
-    </label>
-  );
+const sizeValues = {
+  small: 371,
+  large: 798,
 };
+
+const TextInput = ({ title, name, placeholder, hint, size = 'large' }) => (
+  <Field name={name}>
+    {({ field, form, meta }) => {
+      const validity =
+        meta.touched && !meta.error ? 'success' : meta.touched ? 'failure' : '';
+      return (
+        <label
+          className={`text-input-label ${validity}`}
+          style={{ width: sizeValues[size] }}
+        >
+          {title}
+          <div className="input-container">
+            <input
+              className={validity}
+              {...field}
+              id={name}
+              onFocus={form.handleBlur}
+              placeholder={placeholder}
+            />
+            <img
+              className={validity}
+              src={
+                validity === 'failure'
+                  ? FailureImg
+                  : validity === 'success'
+                  ? SuccessImg
+                  : ''
+              }
+              alt={validity}
+            />
+          </div>
+          <span className="hint">{hint}</span>
+        </label>
+      );
+    }}
+  </Field>
+);
 
 export default TextInput;
