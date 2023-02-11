59979841998689f71bfbc9ce6ae52a2cc436abcf
datodvp <gaguadato98@gmail.com>
2023-02-11 17:39:12 +0400

    validations for experience and educations additional fields

--- src/schema/ValidationSchema.jsx
+++ src/schema/ValidationSchema.jsx
@@ -15,29 +15,157 @@ const validationSchema = Yup.object({
   phone_number: Yup.string()
     .matches(/^(\+995\s?)(5\d{2})\s?\d{2}\s?\d{2}\s?\d{2}$/)
     .required(),
-  experiences: Yup.array(
-    Yup.object({
-      position: Yup.string()
-        .matches(/^.{2,}/)
-        .required(),
-      employer: Yup.string()
-        .matches(/^.{2,}/)
-        .required(),
-      start_date: Yup.date().required(),
-      due_date: Yup.date().required(),
-      description: Yup.string().required(),
-    })
-  ),
-  educations: Yup.array(
-    Yup.object({
-      institute: Yup.string()
-        .matches(/^.{2,}/)
-        .required(),
-      degree_id: Yup.string().required(),
-      due_date: Yup.date().required(),
-      description: Yup.string().required(),
-    })
-  ),
+  experiences: Yup.array()
+    .of(
+      Yup.object().shape(
+        {
+          position: Yup.string().when(
+            ['employer', 'start_date', 'due_date', 'description'],
+            {
+              is: (employer, start_date, due_date, description) =>
+                employer || start_date || due_date || description,
+              then: Yup.string()
+                .matches(/^.{2,}/)
+                .required(),
+            }
+          ),
+          employer: Yup.string().when(
+            ['position', 'start_date', 'due_date', 'description'],
+            {
+              is: (position, start_date, due_date, description) =>
+                position || start_date || due_date || description,
+              then: Yup.string()
+                .matches(/^.{2,}/)
+                .required(),
+            }
+          ),
+          start_date: Yup.date().when(
+            ['position', 'employer', 'due_date', 'description'],
+            {
+              is: (employer, position, due_date, description) =>
+                employer || position || due_date || description,
+              then: Yup.date().required(),
+            }
+          ),
+          due_date: Yup.date().when(
+            ['position', 'employer', 'start_date', 'description'],
+            {
+              is: (employer, position, start_date, description) =>
+                employer || position || start_date || description,
+              then: Yup.date().required(),
+            }
+          ),
+          description: Yup.string().when(
+            ['position', 'employer', 'start_date', 'due_date'],
+            {
+              is: (employer, start_date, due_date, position) =>
+                employer || start_date || due_date || position,
+              then: Yup.string().required(),
+            }
+          ),
+        },
+        [
+          ['position', 'employer'],
+          ['position', 'start_date'],
+          ['position', 'due_date'],
+          ['position', 'description'],
+          ['employer', 'start_date'],
+          ['employer', 'due_date'],
+          ['employer', 'description'],
+          ['start_date', 'due_date'],
+          ['start_date', 'description'],
+          ['due_date', 'description'],
+        ]
+      )
+    )
+    .required()
+    .test((experiences, ctx) => {
+      const Filled = experiences.some((exp) =>
+        Object.values(exp).every((val) => {
+          return !!val;
+        })
+      );
+      if (!Filled) {
+        return ctx.createError({
+          message: 'needs 1 object filled',
+        });
+      }
+      return true;
+    }),
+  educations: Yup.array()
+    .of(
+      Yup.object().shape(
+        {
+          institute: Yup.string().when(
+            ['degree_id', , 'due_date', 'description'],
+            {
+              is: (degree_id, due_date, description) =>
+                degree_id || due_date || description,
+              then: Yup.string().required(),
+            }
+          ),
+          degree_id: Yup.string().when(
+            ['institute', 'due_date', 'description'],
+            {
+              is: (degree_id, due_date, description) =>
+                degree_id || due_date || description,
+              then: Yup.string().required(),
+            }
+          ),
+
+          due_date: Yup.date().when(['institute', 'degree_id', 'description'], {
+            is: (degree_id, due_date, description) =>
+              degree_id || due_date || description,
+            then: Yup.date().required(),
+          }),
+
+          description: Yup.string().when(
+            ['institute', 'degree_id', 'due_date'],
+            {
+              is: (degree_id, due_date, description) =>
+                degree_id || due_date || description,
+              then: Yup.string().required(),
+            }
+          ),
+        },
+        [
+          ['institute', 'degree_id'],
+          ['institute', 'due_date'],
+          ['institute', 'description'],
+          ['degree_id', 'due_date'],
+          ['degree_id', 'description'],
+          ['due_date', 'description'],
+        ]
+      )
+    )
+    .required()
+    .test((educations, ctx) => {
+      const Filled = educations.some((exp) =>
+        Object.values(exp).every((val) => {
+          return !!val;
+        })
+      );
+      if (!Filled) {
+        return ctx.createError({
+          message: 'needs 1 object filled',
+        });
+      }
+      return true;
+    }),
 });
 
 export default validationSchema;
+
+///// EXPERIENCES
+
+// Yup.object({
+//   position: Yup.string()
+//     .matches(/^.{2,}/)
+//     .required(),
+//   employer: Yup.string()
+//     .matches(/^.{2,}/)
+//     .required(),
+//   start_date: Yup.date().required(),
+//   due_date: Yup.date().required(),
+//   description: Yup.string().required(),
+// })
