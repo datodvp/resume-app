ee09f187febc23468cf45415d60b7c173b052083
datodvp <gaguadato98@gmail.com>
2023-02-12 18:07:13 +0400

    fix: imageInput warnnings

--- src/pages/PersonForm/components/ImageInput/ImageInput.jsx
+++ src/pages/PersonForm/components/ImageInput/ImageInput.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Field } from 'formik';
 import { UserContext } from '../../../../context/UserContext';
 
